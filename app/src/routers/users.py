21fc6f91fc9e94cb6bdd457f543b39253de29354
Vitor <vitor101bl9@gmail.com>
2022-12-26 13:46:04 -0300

    feat: Validate the user information

--- app/src/routers/users.py
+++ app/src/routers/users.py
@@ -1,24 +1,36 @@
 from typing import List
-from uuid import UUID
+from uuid import UUID, uuid4
+
+from fastapi.responses import JSONResponse
 from user_models import User
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
 
-user: List[User] = [
+user_list: List[User] = [
     User(
+        id=UUID("440b410e-9192-4e15-bd59-234e933b411b"),
         first_name="Vitor Augusto",
+        email="vitor@vitor.com",
         last_name="Guimaraes",
         age=19,
         year=2003
     )
 ]
+print(user_list[0].id)
 
 
 @router.post("/api/v1/user")
 async def create_new_user(new_user: User):
-    ...
+    if any(new_user.email == user.email for user in user_list):
+        raise HTTPException(status_code=409, detail={
+            "error": "User already exists or Invalid email"
+        })
+    user_list.append(new_user)
+    return JSONResponse(status_code=200, content={
+        "success": "User created successfully"
+    })
 
 @router.get("/api/v1/user/{id}")
 async def get_user(id: UUID):
