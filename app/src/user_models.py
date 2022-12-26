76142e8c296786e6c1fae8a01f2611fdfb46dd4c
Vitor <vitor101bl9@gmail.com>
2022-12-26 12:43:35 -0300

    feat: Create a new user model

---
+++ app/src/user_models.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    middle_name: Optional[str] = None
+    last_name: str
+    age: int
+    year: int
