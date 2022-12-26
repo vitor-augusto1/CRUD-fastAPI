71ca0fb47f2631435d6e0df22d51e1d9d4680d4b
Vitor <vitor101bl9@gmail.com>
2022-12-26 13:45:29 -0300

    feat: Add email to the User model

--- app/src/user_models.py
+++ app/src/user_models.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 class User(BaseModel):
     id: Optional[UUID] = uuid4()
     first_name: str
+    email: str
     middle_name: Optional[str] = None
     last_name: str
     age: int
