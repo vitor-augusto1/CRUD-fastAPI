39a388c7138d358d9137b554d7eadbf463417282
Vitor <vitor101bl9@gmail.com>
2022-12-28 15:44:07 -0300

    feat: Add a new patch http method handler; update partial information

--- app/src/routers/users.py
+++ app/src/routers/users.py
@@ -1,16 +1,17 @@
 from typing import List
-from uuid import UUID, uuid4
+from uuid import UUID
 
 from fastapi.responses import JSONResponse
-from user_models import User
+from user_models import UserBase, UserCreate, UserOptional
 
 from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
 
-user_list: List[User] = [
-    User(
+user_list: List[UserBase] = [
+    UserCreate(
         id=UUID("440b410e-9192-4e15-bd59-234e933b411b"),
+        password="asdjcajkdbvjkdvjadcv",
         first_name="Vitor Augusto",
         email="vitor@vitor.com",
         last_name="Guimaraes",
@@ -21,7 +22,7 @@ user_list: List[User] = [
 
 
 @router.post("/api/v1/user")
-async def create_new_user(new_user: User):
+async def create_new_user(new_user: UserCreate):
     if any(new_user.email == user.email for user in user_list):
         raise HTTPException(status_code=409, detail={
             "error": "User already exists or Invalid email"
@@ -39,10 +40,11 @@ async def get_user(user_id: UUID):
 
 
 @router.put("/api/v1/user/{user_id}")
-async def update_user(user_id: UUID, new_user_information: User):
+async def update_user(user_id: UUID, new_user_information: UserCreate):
     for user in user_list:
         if user.id == user_id:
-            user = new_user_information
+            i = user_list.index(user)
+            user_list[i] = new_user_information
             return JSONResponse(status_code=200, content={
                 "success": "User updated successfully"
             })
@@ -52,8 +54,18 @@ async def update_user(user_id: UUID, new_user_information: User):
 
 
 @router.patch("/api/v1/user/{user_id}")
-def update_user_information(user_id: UUID, new_user_information: User):
-    ...
+async def update_user_information(user_id: UUID, new_user_information: UserOptional):
+    for stored_user in user_list:
+        if stored_user.id == user_id:
+            stored_user_model = UserOptional(**dict(stored_user))
+            new_user_data = new_user_information.dict(exclude_unset=True)
+            updated_User = stored_user_model.copy(update=new_user_data)
+            i = user_list.index(stored_user)
+            user_list[i] = updated_User
+            return updated_User
+    raise HTTPException(status_code=400, detail={
+        "error": "User not found"
+    })
 
 
 @router.delete("/api/v1/user/{id}")
