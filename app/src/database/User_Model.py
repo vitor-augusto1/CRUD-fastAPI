00663d52205a5af0b6b5f292d16821464cb613d8
vitor-augusto1 <v1t0r4ug@gmail.com>
2022-12-30 20:53:26 -0300

    Create all metadatas for User model in database

--- app/src/database/User_Model.py
+++ app/src/database/User_Model.py
@@ -1,18 +1,19 @@
 from sqlalchemy import Column, Integer, String
-from .connection import Base
+from .connection import Base, engine
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(String(100), unique=True, primary_key=True)
-    first_name = Column(String(15))
+    first_name = Column(String(50))
     email = Column(String(100), unique=True)
     password = Column(String(90))
-    middle_name = Column(String(15), default=None)
-    last_name = Column(String(15))
+    middle_name = Column(String(100), default=None)
+    last_name = Column(String(100))
     age = Column(Integer)
     year = Column(Integer)
 
     def __repr__(self) -> str:
         return f'{self.first_name} {self.last_name} - {self.id}'
 
+User.metadata.create_all(bind=engine)
