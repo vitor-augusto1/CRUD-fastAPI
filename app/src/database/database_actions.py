749f480c38b7da64beca2aa2858a83630502e1a0
vitor-augusto1 <v1t0r4ug@gmail.com>
2022-12-30 20:52:54 -0300

    Import the UserCreate pydantic schema from user_schema

--- app/src/database/database_actions.py
+++ app/src/database/database_actions.py
@@ -2,11 +2,14 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 
 from .User_Model import User
+from user_schema import UserCreate
 
 def get_user_by_id(database_session: Session, user_id: UUID):
     return database_session.query(User).filter(User.id == user_id).first()
+
 def get_user_by_email(database_session: Session, user_email: str):
     return database_session.query(User).filter(User.email == user_email).first()
+
 def create_new_user(database_session: Session, new_user: UserCreate):
     new_user = User(
         id=new_user.id,
