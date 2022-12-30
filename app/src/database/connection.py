28306ec667daa82d7e7e7b179cbfc5997f10812e
vitor-augusto1 <v1t0r4ug@gmail.com>
2022-12-30 19:29:38 -0300

    refactor: Changes again the session name

--- app/src/database/connection.py
+++ app/src/database/connection.py
@@ -14,5 +14,6 @@ url_object = URL.create(
 
 engine = create_engine(url_object)
 
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
