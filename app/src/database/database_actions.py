e72f6158c43caabe880f7e8180e8fa7a55ec25d2
vitor-augusto1 <v1t0r4ug@gmail.com>
2022-12-30 20:52:02 -0300

    feat: Add new function `create_new_user` to the database_actions

--- app/src/database/database_actions.py
+++ app/src/database/database_actions.py
@@ -7,3 +7,18 @@ def get_user_by_id(database_session: Session, user_id: UUID):
     return database_session.query(User).filter(User.id == user_id).first()
 def get_user_by_email(database_session: Session, user_email: str):
     return database_session.query(User).filter(User.email == user_email).first()
+def create_new_user(database_session: Session, new_user: UserCreate):
+    new_user = User(
+        id=new_user.id,
+        first_name=new_user.first_name,
+        email=new_user.email,
+        password=new_user.password,
+        middle_name=new_user.middle_name,
+        last_name=new_user.last_name,
+        age=new_user.age,
+        year=new_user.year
+    )
+    database_session.add(new_user)
+    database_session.commit()
+    database_session.refresh(new_user)
+    return new_user
