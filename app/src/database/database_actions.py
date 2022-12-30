3df4739537364498464325c491a9e2cb4f235cae
vitor-augusto1 <v1t0r4ug@gmail.com>
2022-12-30 20:51:30 -0300

    feat: Add new function `get_user_by_email` to the database_actions

--- app/src/database/database_actions.py
+++ app/src/database/database_actions.py
@@ -5,3 +5,5 @@ from .User_Model import User
 
 def get_user_by_id(database_session: Session, user_id: UUID):
     return database_session.query(User).filter(User.id == user_id).first()
+def get_user_by_email(database_session: Session, user_email: str):
+    return database_session.query(User).filter(User.email == user_email).first()
