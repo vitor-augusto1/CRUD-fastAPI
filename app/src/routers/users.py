a0901f31af8700e062cda6b414326fc1f3bf2e15
vitor-augusto1 <v1t0r4ug@gmail.com>
2022-12-30 20:50:15 -0300

    Import Session class to add type to the route parameter

--- app/src/routers/users.py
+++ app/src/routers/users.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
 from user_schema import UserBase, UserCreate, UserOptional
 from database.connection import SessionLocal, engine
