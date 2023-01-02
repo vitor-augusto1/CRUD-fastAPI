7840aea816a87085740493131b7fb1a281ee38f4
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-02 11:37:49 -0300

    Import schemas

--- app/src/database/database_actions.py
+++ app/src/database/database_actions.py
@@ -2,13 +2,16 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 
 from .User_Model import User
-from user_schema import UserCreate
+from user_schema import UserBase, UserCreate, UserOptional
+
 
 def get_user_by_id(database_session: Session, user_id: UUID):
     return database_session.query(User).filter(User.id == user_id).first()
 
+
 def get_user_by_email(database_session: Session, user_email: str):
     return database_session.query(User).filter(User.email == user_email).first()
+
 
 def create_new_user(database_session: Session, new_user: UserCreate):
     new_user = User(
