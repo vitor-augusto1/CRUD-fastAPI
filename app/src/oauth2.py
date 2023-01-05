8fdd24cd641842ef914a37c6effcfffa99c41b12
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-04 21:36:07 -0300

    feat: Add new get_current_user for endpoint security; Checks if the user is authenticated

---
+++ app/src/oauth2.py
@@ -0,0 +1,14 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from utils.token import verify_jwt_token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return verify_jwt_token(token, credentials_exception)
