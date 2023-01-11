6f1f12a0ff5f7f48353276ff5be7ca351d88eb52
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-11 14:35:09 -0300

    refactor: Change the email validation function name to `is_a_valid_email`

--- app/src/utils/email_validation.py
+++ app/src/utils/email_validation.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException, status
 
 email_regex_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
 
-def check_email_validation(email: str) -> bool:
+def is_a_valid_email(email: str) -> bool:
     if re.fullmatch(email_regex_pattern, email):
         return True
     else:
