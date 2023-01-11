324df2bafdd1d71c18a8ee7f19b4700e6fc0546d
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-11 10:16:22 -0300

    refactor: Add builtin fastAPI status code to authentication route

--- app/src/routers/authentication.py
+++ app/src/routers/authentication.py
@@ -5,7 +5,7 @@ from database.User_Model import User
 from utils.verify_password import verify_user_password
 from utils.token import create_jwt_access_token
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from database.connection import SessionLocal
 from database import database_actions
@@ -21,11 +21,11 @@ def login(login_request: OAuth2PasswordRequestForm = Depends(),
           database: Session = Depends(database_actions.get_database)):
     user = database.query(User).filter(User.email == login_request.username).first()
     if not user:
-        raise HTTPException(status_code=404, detail={
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={
             "error": "Invalid Credentials"
         })
     if not verify_user_password(user.password, login_request.password):
-        raise HTTPException(status_code=404, detail={
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={
             "error": "Invalid Credentials"
         })
     access_token = create_jwt_access_token(data={"sub": user.email})
