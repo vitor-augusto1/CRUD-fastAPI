bfb1b50587d3578430ae49dfeacccd24c97e66ef
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-16 19:24:07 -0300

    feat: Add new test to check status code on already existent email

--- app/src/test/test_users.py
+++ app/src/test/test_users.py
@@ -1,5 +1,6 @@
 import httpx
 import json
+
 class TestCreateNewUser:
     def test_should_return_400_bad_request_on_invalid_email(self):
         data = {
@@ -15,3 +16,18 @@ class TestCreateNewUser:
         response = httpx.post("http://localhost:8000/api/v1/user/", content=json.dumps(data), headers=headers)
         assert response.status_code == 400
         assert response.json() == {'detail': {'error': 'Invalid Email'}}
+
+    def test_should_return_400_bad_request_on_email_already_exists(self):
+        data = {
+            "first_name": "New User",
+            "email": "alredytaken@email.com",
+            "middle_name": "User new",
+            "last_name": "Last Name",
+            "age": 19,
+            "year": 2003,
+            "password": "new_password"
+        }
+        headers = {'Content-Type': 'application/json'}
+        response = httpx.post("http://localhost:8000/api/v1/user/", content=json.dumps(data), headers=headers)
+        assert response.status_code == 400
+        assert response.json() == {'detail': {'error': 'Email already registered'}}
