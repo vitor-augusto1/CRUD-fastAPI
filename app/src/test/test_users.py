a338fb5792860051c0f897d8a453b84735f67d5e
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-16 19:23:38 -0300

    feat: Add new test class to test user creation; check the status code on invalid credentials

---
+++ app/src/test/test_users.py
@@ -0,0 +1,17 @@
+import httpx
+import json
+class TestCreateNewUser:
+    def test_should_return_400_bad_request_on_invalid_email(self):
+        data = {
+            "first_name": "New User",
+            "email": "alredytakenemailcom",
+            "middle_name": "User new",
+            "last_name": "Last Name",
+            "age": 19,
+            "year": 2003,
+            "password": "new_password"
+        }
+        headers = {'Content-Type': 'application/json'}
+        response = httpx.post("http://localhost:8000/api/v1/user/", content=json.dumps(data), headers=headers)
+        assert response.status_code == 400
+        assert response.json() == {'detail': {'error': 'Invalid Email'}}
