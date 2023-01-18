3b70d70fb787413eb4c7f1164a18f7681c733932
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-18 16:06:23 -0300

    feat: Remove some fields from the data object to test the endpoint

--- app/src/test/test_users.py
+++ app/src/test/test_users.py
@@ -34,15 +34,13 @@ class TestCreateNewUser:
         assert response.status_code == 400
         assert response.json() == {'detail': {'error': 'Email already registered'}}
 
-    def test_should_return_200_on_successful_user_creation(self):
+
+    def test_should_return_422_unprocessable_entity_on_missing_required_fields(self):
         random_uuid = uuid4()
         data = {
-            "first_name": "Test user",
             "email": f"test+{random_uuid}@email.com",
             "middle_name": "Test",
-            "last_name": "Test",
             "age": 19,
-            "year": 2003,
             "password": "new_password"
         }
         headers = {'Content-Type': 'application/json'}
@@ -51,7 +49,7 @@ class TestCreateNewUser:
             content=json.dumps(data),
             headers=headers
         )
-        assert response.status_code == 200
+        assert response.status_code == 422
 
     def test_should_return_422_unprocessable_entity_on_missing_required_fields(self):
         random_uuid = uuid4()
