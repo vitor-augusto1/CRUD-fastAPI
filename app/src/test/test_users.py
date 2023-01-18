1677dafddf4ab2f749d128f2a88dfd2bfd32a5cc
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-18 15:15:57 -0300

    feat: Add new test to check if it returns 200 status code on valid credentials at `TestCreateNewUser` test class

--- app/src/test/test_users.py
+++ app/src/test/test_users.py
@@ -1,5 +1,7 @@
-import httpx
+from uuid import uuid4
 import json
+
+import httpx
 
 class TestCreateNewUser:
     def test_should_return_400_bad_request_on_invalid_email(self):
@@ -31,3 +33,23 @@ class TestCreateNewUser:
         response = httpx.post("http://localhost:8000/api/v1/user/", content=json.dumps(data), headers=headers)
         assert response.status_code == 400
         assert response.json() == {'detail': {'error': 'Email already registered'}}
+
+    def test_should_return_200_on_successful_user_creation(self):
+        random_uuid = uuid4()
+        data = {
+            "first_name": "Test user",
+            "email": f"test+{random_uuid}@email.com",
+            "middle_name": "Test",
+            "last_name": "Test",
+            "age": 19,
+            "year": 2003,
+            "password": "new_password"
+        }
+        headers = {'Content-Type': 'application/json'}
+        response = httpx.post(
+            "http://localhost:8000/api/v1/user/",
+            content=json.dumps(data),
+            headers=headers
+        )
+        assert response.status_code == 200
+
