fc7405b5b13fdd00fda48046ccc73985424b0661
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-20 10:06:01 -0300

    feat: Add new function test to check the status code on invalid credentials at user login route

--- app/src/test/test_authentication.py
+++ app/src/test/test_authentication.py
@@ -2,3 +2,12 @@ from fastapi import HTTPException, status
 import httpx
 from utils.token import verify_jwt_token
 
+def test_should_return_404_not_found_on_invalid_credentials():
+    response = httpx.post(
+        "http://localhost:8000/login",
+        data={"username": "test@test.com", "password": "password"},
+        headers={'Content-Type': 'application/x-www-form-urlencoded'}
+    )
+    assert response.status_code == 404
+    assert response.json() == {'detail': {'error': 'Invalid Credentials'}}
+
