cbfa0d379dfdcaf9dd6ba3dcb21bea4800a43921
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-25 12:59:54 -0300

    feat: Add dot env to hide the DB credentials and the JWT secret

--- app/src/utils/email_validation.py
+++ app/src/utils/email_validation.py
@@ -8,8 +8,7 @@ email_regex_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 def is_a_valid_email(email: str) -> bool:
     if re.fullmatch(email_regex_pattern, email):
         return True
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail={'error': 'Invalid Email'},
-        )
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail={'error': 'Invalid Email'},
+    )
--- app/src/utils/token.py
+++ app/src/utils/token.py
@@ -1,11 +1,16 @@
+import os
 from datetime import datetime, timedelta
 from typing import Optional
 
+from dotenv import load_dotenv
 from fastapi import HTTPException
 from jose import JWTError, jwt
-from token_schema import Token, TokenData
 
-SECRET_KEY = '2c1f5d3baf228e5cd69d15df429ad032fea06f5d44ba2c773c4a710507e9ce0c'
+from token_schema import TokenData
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 120
 
@@ -21,13 +26,15 @@ def create_jwt_access_token(
             minutes=ACCESS_TOKEN_EXPIRE_MINUTES
         )
     data_to_encode.update({'exp': expire})
-    encoded_jwt = jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(
+        data_to_encode, str(SECRET_KEY), algorithm=ALGORITHM
+    )
     return encoded_jwt
 
 
 def verify_jwt_token(token: str, credentials_exception: HTTPException):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, str(SECRET_KEY), algorithms=[ALGORITHM])
         email = payload.get('sub')
         if email is None:
             raise credentials_exception
