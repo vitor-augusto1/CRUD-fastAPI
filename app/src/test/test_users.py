41db3e47e51bb3a89362aef498f245ac4c890012
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-25 09:33:44 -0300

    refactor: Change the values to class atribute

--- app/src/test/test_users.py
+++ app/src/test/test_users.py
@@ -18,8 +18,7 @@ class TestCreateNewUser:
             "year": 2003,
             "password": "new_password"
         }
-        headers = {'Content-Type': 'application/json'}
-        response = httpx.post("http://localhost:8000/api/v1/user/", content=json.dumps(data), headers=headers)
+        response = httpx.post(self.url, content=json.dumps(data), headers=self.json_header)
         assert response.status_code == 400
         assert response.json() == {'detail': {'error': 'Invalid Email'}}
 
@@ -33,8 +32,7 @@ class TestCreateNewUser:
             "year": 2003,
             "password": "new_password"
         }
-        headers = {'Content-Type': 'application/json'}
-        response = httpx.post("http://localhost:8000/api/v1/user/", content=json.dumps(data), headers=headers)
+        response = httpx.post(self.url, content=json.dumps(data), headers=self.json_header)
         assert response.status_code == 400
         assert response.json() == {'detail': {'error': 'Email already registered'}}
 
@@ -46,11 +44,10 @@ class TestCreateNewUser:
             "age": 19,
             "password": "new_password"
         }
-        headers = {'Content-Type': 'application/json'}
         response = httpx.post(
-            "http://localhost:8000/api/v1/user/",
+            self.url,
             content=json.dumps(data),
-            headers=headers
+            headers=self.json_header
         )
         assert response.status_code == 422
 
@@ -61,15 +58,14 @@ class TestCreateNewUser:
             "email": f"test+{now.second}+test@email.com",
             "middle_name": "Test",
             "last_name": "Test",
-            "age": 19,
-            "year": 2003,
+            "age": 20,
+            "year": 2002,
             "password": f"{now.day}+{now.hour}"
         }
-        headers = {'Content-Type': 'application/json'}
         response = httpx.post(
-            "http://localhost:8000/api/v1/user/",
+            self.url,
             content=json.dumps(data),
-            headers=headers
+            headers=self.json_header
         )
         assert response.status_code == 200
 
@@ -78,29 +74,28 @@ class TestGetUserInformation:
     url = "http://localhost:8000/api/v1/user/me"
     login_url = "http://localhost:8000/login"
     url_encoder_header = {'Content-Type': 'application/x-www-form-urlencoded'}
+
     def test_should_return_401_unauthorized_on_invalid_JWT_token(self):
         headers = {
             "Authorization": "Bearer XXYxWv4Z4-L5K3nJvejYpNP8rjiMIfAjs"
         }
         response = httpx.get(
-            "http://localhost:8000/api/v1/user/me",
+            self.url,
             headers=headers
         )
         assert response.status_code == 401
         assert response.json() == {'detail': 'Could not validate credentials'}
 
     def test_should_return_401_unauthorized_when_user_does_not_send_the_token(self):
-        response = httpx.get(
-            "http://localhost:8000/api/v1/user/me"
-        )
+        response = httpx.get(self.url)
         assert response.status_code == 401
         assert response.json() == {"detail": "Not authenticated"}
 
     def test_should_return_200_ok_on_valid_token(self):
         response = httpx.post(
-            "http://localhost:8000/login",
+            self.login_url,
             data={"username": "new@new.com", "password": "123"},
-            headers={'Content-Type': 'application/x-www-form-urlencoded'}
+            headers=self.url_encoder_header
         )
         response_json = response.json()
         jwt_token = response_json.get('access_token')
@@ -108,7 +103,7 @@ class TestGetUserInformation:
             "Authorization": f"Bearer {jwt_token}"
         }
         response = httpx.get(
-            "http://localhost:8000/api/v1/user/me",
+            self.url,
             headers=headers
         )
         assert response.status_code == 200
@@ -118,6 +113,7 @@ class TestUpdateUserInformation:
     url = "http://localhost:8000/api/v1/user/me"
     login_url = "http://localhost:8000/login"
     url_encoder_header = {'Content-Type': 'application/x-www-form-urlencoded'}
+
     def test_should_return_401_unauthorized_on_invalid_JWT_token(self):
         headers = {
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5c.DU2NTkzMX0",
@@ -132,7 +128,7 @@ class TestUpdateUserInformation:
             "year": 2003
         }
         response = httpx.put(
-            "http://localhost:8000/api/v1/user/me",
+            self.url,
             content=json.dumps(new_user_data),
             headers=headers
         )
@@ -141,9 +137,9 @@ class TestUpdateUserInformation:
 
     def test_should_return_200_ok_on_successful_update(self):
         login_response = httpx.post(
-            "http://localhost:8000/login",
+            self.login_url,
             data={"username": "new@new.com", "password": "123"},
-            headers={'Content-Type': 'application/x-www-form-urlencoded'}
+            headers=self.url_encoder_header
         )
         response_json = login_response.json()
         jwt_token = response_json.get('access_token')
@@ -160,7 +156,7 @@ class TestUpdateUserInformation:
             "year": 2003
         }
         response = httpx.put(
-            "http://localhost:8000/api/v1/user/me",
+            self.url,
             content=json.dumps(new_user_data),
             headers=headers
         )
@@ -169,9 +165,9 @@ class TestUpdateUserInformation:
 
     def test_should_return_200_ok_on_successful_partial_update(self):
         login_response = httpx.post(
-            "http://localhost:8000/login",
+            self.login_url,
             data={"username": "new@new.com", "password": "123"},
-            headers={'Content-Type': 'application/x-www-form-urlencoded'}
+            headers=self.url_encoder_header
         )
         response_json = login_response.json()
         jwt_token = response_json.get('access_token')
@@ -185,7 +181,7 @@ class TestUpdateUserInformation:
             "year": 2002
         }
         response = httpx.patch(
-            "http://localhost:8000/api/v1/user/me",
+            self.url,
             content=json.dumps(new_user_data),
             headers=headers
         )
