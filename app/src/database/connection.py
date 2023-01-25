c800651291121d439c85d152983ad60a76d27449
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-01-25 13:01:06 -0300

    Add dot env

--- app/src/database/connection.py
+++ app/src/database/connection.py
@@ -1,15 +1,20 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
+
 url_object = URL.create(
     drivername='mysql+pymysql',
-    username='root',
-    password='root',
-    host='localhost',
-    database='Users',
-    port=3306,
+    username=os.getenv('DB_USERNAME'),
+    password=os.getenv('DB_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    database=os.getenv('DB_NAME'),
+    port=os.getenv('DB_PORT'),
 )
 
 engine = create_engine(url_object)
